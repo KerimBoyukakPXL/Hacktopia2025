469adfb2c447161474db0605763dc336f52c651a
KerimBoyukakPXL <93250096+KerimBoyukakPXL@users.noreply.github.com>
2025-03-01 13:09:00 +0100

    update

---
+++ frontend/freshdash-app/src/app/core/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  location: string = '';
+
+  constructor(private router: Router) {}
+
+  search() {
+    this.router.navigate(['/location-detail'], { queryParams: { location: this.location } });
+  }
+}
